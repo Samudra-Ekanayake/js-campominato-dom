2b4690ec07145526db039f651ae41dfb83235a69
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-02 20:22:51 +0200

    M1

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
+
+
 let griglia = document.getElementById("grid");
-
-
-
-for (let i = 1; i <= 100; i++) {
-
-    let elemento = creaQuadrato(i)
-    griglia.append(elemento);
-}
+let bombe = generaBombe();
 
 
 function creaQuadrato(numero) {
@@ -15,11 +10,34 @@ function creaQuadrato(numero) {
     square.innerText = numero;
 
     square.addEventListener("click", function () {
-        console.log("cliccato", this)
-        this.classList.toggle("evidenziata")
-
+        if (!bombe.includes(parseInt(this.innerText))){
+            this.classList.toggle("evidenziata")
+        }
     });
 
     return square;
+}
+
+function generaBombe() {
+    let bombe = [];
+    let numeroCasuale;
+
+    while (bombe.length < 16) {
+        numeroCasuale = Math.floor(Math.random() * 100) + 1;
+    }
+
+    if (!bombe.includes(numeroCasuale)) {
+        bombe.push(numeroCasuale)
+    };
+    return bombe;
+}
+
+for (let i = 1; i <= 100; i++) {
+    let elemento = creaQuadrato(i);
+    if (bombe.includes(i)) {
+        elemento.classList.add("bomba");
+    }
+
+    griglia.append(elemento);
 }
 
