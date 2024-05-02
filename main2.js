67f535c855bd8083d869404229f13a727db3c3d4
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-02 21:07:16 +0200

    M1

--- main2.js
+++ main2.js
@@ -1,8 +1,5 @@
-
-
 let griglia = document.getElementById("grid");
 let bombe = generaBombe();
-
 
 function creaQuadrato(numero) {
     let square = document.createElement("div");
@@ -10,13 +7,18 @@ function creaQuadrato(numero) {
     square.innerText = numero;
 
     square.addEventListener("click", function () {
+        // Verifica se il numero della casella cliccata è una bomba
         if (bombe.includes(numero)) {
+            // Se è una bomba, applica la classe "bomba"
             this.classList.add("bomba");
         } else {
+            // Altrimenti, verifica se la casella è già stata cliccata
             if (!this.classList.contains("evidenziata")) {
+                // Se non è stata cliccata, applica la classe "evidenziata" per evidenziarla
                 this.classList.add("evidenziata");
             } else {
-                this.classList.remove("evidenziata")
+                // Se è già stata cliccata, rimuovi la classe "evidenziata" per disattivarne l'evidenziazione
+                this.classList.remove("evidenziata");
             }
         }
     });
@@ -30,7 +32,8 @@ function generaBombe() {
 
     while (bombe.length < 16) {
         numeroCasuale = Math.floor(Math.random() * 100) + 1;
-
+        
+        // Verifica se il numero casuale è già presente nell'array delle bombe
         if (!bombe.includes(numeroCasuale)) {
             bombe.push(numeroCasuale);
         }
@@ -39,9 +42,8 @@ function generaBombe() {
     return bombe;
 }
 
+// Ciclo per creare la griglia di quadrati
 for (let i = 1; i <= 100; i++) {
     let elemento = creaQuadrato(i);
     griglia.append(elemento);
 }
-
-
